8e6385b79037f89b990977548b84542bd8eafedf
noco04 <37374947+noco04@users.noreply.github.com>
2018-11-22 11:12:02 +0100

    Update Programma_Noah_Auto.ino

--- Programma_Noah_Auto.ino
+++ Programma_Noah_Auto.ino
@@ -1,34 +1,24 @@
 
 //pinnummers toewijzen
 const int PWM_Pin = 3;
-const int ControlPinA = 4;  
-const int ControlPinB = 3;
+const int ControlPinA = 1;  
+const int ControlPinB = 2;
 int val=0;
-int valdisplay=0;
 //motorwaarde instellen, min 0, max 255
 int PB = 0;
-byte numDigits = 4;
 
-byte digitPins[] = {14, 5, 6, 4};
-
-byte segmentPins[] = {13, 7, 8, 9, 10, 11, 12, 14};
-
-SevSeg sevseg; //Initiate a seven segment controller object
 
 
 
 void setup() {
 
-sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
 
-sevseg.setBrightness(50);
 
   Serial.begin(9600);
   pinMode (0, INPUT_PULLUP); //uit voorzorg
   pinMode (PWM_Pin, OUTPUT);
   pinMode (ControlPinA, OUTPUT);
   pinMode (ControlPinB, OUTPUT);
-  pinMode (11,OUTPUT);
   digitalWrite(PWM_Pin, LOW);
   
   
@@ -48,10 +38,6 @@ void loop() {
     int sensorValue = analogRead(A2);
     Serial.println(sensorValue);
     val = map(sensorValue, 0, 1024, 0, 255);
-    analogWrite (11,val);
     analogWrite (PWM_Pin,val);
-    valdisplay = map(val, 0, 255, 0, 51);
-     sevseg.setNumber(valdisplay, 0);
-sevseg.refreshDisplay(); // Must run repeatedly
     delay(5);
 }
